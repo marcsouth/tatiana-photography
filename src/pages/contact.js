b212096724392e2003f655891a11464e9dca4175
marcsouth <104800775+marcsouth@users.noreply.github.com>
2023-02-22 16:24:51 -0500

    visual changes with tatiana

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,16 +10,19 @@ export default function contact() {
 
 
 
-  <div id="demo-text">CONTACT US</div>
+  <div id="demo-text">ENQUIRY FORM</div>
 
+
+  <p className='contactpabove'>To enquire about fine art prints and booking, please contact me below.</p>
 
 
   <form method="POST" action="https://formsubmit.co/el/duduxe">
 
+
       <div className="contactdivide">
 
-     <input type="text" name="name" placeholder="Name" required />
-     <input type="text" name="lastname" placeholder="Last Name" className='emailmiddle' required />
+     <input type="text" name="name" placeholder="Name*" required />
+     <input type="text" name="lastname" placeholder="Last Name" className='emailmiddle' />
 
       </div>
      
@@ -28,10 +31,13 @@ export default function contact() {
      <input type="hidden" name="_template" value="table" />
 
 
-      <input type="email" name="email" placeholder="Email"  required />
-     <textarea type="message" name="message" placeholder="Your Message" className='messagefield' required />
+      <input type="email" name="email" placeholder="Email*"  required />
+     <textarea type="message" name="message" placeholder="Your Message*" className='messagefield' required />
      <button type="submit" value="Send">Send</button>
     </form>
+
+
+<p className='contactp'>By submitting this form you will be added to our mailing list.</p>
 
 <Footer />
 
